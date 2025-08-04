0846bd2d9819b2707d99b71b1dc4a04a45713990
Elimurray <eli.murr04@gmail.com>
2025-08-04 18:37:23 +1200

    updated graph colour

--- frontend/present.js
+++ frontend/present.js
@@ -33,8 +33,8 @@ const myChart = new Chart(ctx, {
       {
         label: "Sample Data",
         data: values,
-        backgroundColor: "rgba(75, 192, 192, 0.2)",
-        borderColor: "rgba(75, 192, 192, 1)",
+        backgroundColor: "#b0bfffff",
+        borderColor: "#3a56d4",
         borderWidth: 1,
       },
     ],
